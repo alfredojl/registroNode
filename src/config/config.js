e9d8091ad33ec5c8577ca8a383d688cdaac4669a
alfredojl <alfredo.jl323@gmail.com>
2020-10-19 14:07:56 -0500

    Ya

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,8 @@
 const config = {
-    api: 'http://localhost:3000',
+    // Para localhost
+    // api: 'http://localhost:3000',
+    // Para producción
+    api: 'http://172.26.60.61:3000'
 }
 
 module.exports = config;
